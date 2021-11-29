77a449d1e73a1852a12159a084a5ad176b4151a4
Politary <sergey.2000.kambur@gmail.com>
2021-11-29 04:55:23 +0200

    Updates

---
+++ precache-manifest.0a5fd7237d334b197ba37b9b2e6d1fad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95eefa88607f27a0ce97108582444814",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "dbd2fb7c87655f20d31f",
+    "url": "/weather/static/css/main.86d3611b.chunk.css"
+  },
+  {
+    "revision": "296f3af5911870eec76c",
+    "url": "/weather/static/js/2.5e417db2.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/weather/static/js/2.5e417db2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbd2fb7c87655f20d31f",
+    "url": "/weather/static/js/main.e6596085.chunk.js"
+  },
+  {
+    "revision": "65a135568c88d392bf25",
+    "url": "/weather/static/js/runtime-main.d8725fa2.js"
+  },
+  {
+    "revision": "da5cc546a96de557ec97be600b82b4f2",
+    "url": "/weather/static/media/Error.da5cc546.svg"
+  },
+  {
+    "revision": "c2a17a15771be8c029079ffac9d6b0e8",
+    "url": "/weather/static/media/Search.c2a17a15.svg"
+  }
+]);
